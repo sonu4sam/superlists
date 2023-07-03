1a6b887235e528697201cb364c439a1c6189c4f3
sonu4sam <sonu4sam@outlook.com>
2023-07-03 16:35:42 -0500

    unittest pass

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
-        
+    def test_root_url_equal_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
