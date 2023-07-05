9f84e836fe5070afca95b5c7e1ba8ea940d729ce
sonu4sam <sonu4sam@outlook.com>
2023-07-05 00:03:48 -0500

    latest

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,78 +1,114 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait  # instead of time 
-from selenium.webdriver.support import expected_conditions as EC # 
-from django.db import models
-import unittest
-from time import sleep
-from selenium.common.exceptions import NoSuchElementException
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+import time
+
+MAX_WAIT = 10
 
 
-class NewVisitorTest(unittest.TestCase):
-    
-    @classmethod
-    def setUp(cls):
-        options = Options()
-        options.headless = False # use true if you want the browser not to be displayed.
-        cls.browser = webdriver.Firefox(options=options)
-    
-    @classmethod    
-    def tearDown(cls):
-        cls.browser.quit()
-        
-    def check_for_row_in_list_table(self, row_text):
-        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'id_list_table')))
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        try:
-            rows = table.find_elements(By.TAG_NAME, 'tr')
-        except NoSuchElementException:
-            self.assertEqual(len(rows), 0)
-            
-        self.assertIn(row_text, [row.text for row in rows])
-        
-    def test_can_start_a_list_and_retreive_later(self):
-        # Samantha has heard about a cool new online to-do app. She goes to checkout the home page
-        self.browser.get('http://localhost:8000')
-        
-        # she notices the page title and header mention to-do lists
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
+    def test_can_start_a_list_for_one_user(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1')
-        self.assertIn('your to-do list', header_text.text.lower())
-        
-        # she is invited to enter a to-do list item straight away
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-        
-        # she types "Buy Groceries" into a text box
-        inputbox.send_keys('Buy Groceries')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        sleep(1)
-        
-        # When she hits enter, the page updates, and now the page lists "1. Buy Groceries" as an item in a to-do list.
-        self.check_for_row_in_list_table('1: Buy Groceries')
-        
-        # There is still a text box inviting her to add another item. She enters "Make a meal plan" (she is very methodical)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('Make a meal plan')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        sleep(1)
-        
+
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy Groceries')
-        self.check_for_row_in_list_table('2: Make a meal plan')
-        
-        # She wonders whether the site will remember her list. The she sees that the site has generated an unique URL for her -- there is some explanatory text to that effect. 
-        
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Satisfied, she goes back to sleep
 
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith start a new todo list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # She visits that URL - her to-do list is still there. 
-        self.fail('Finish the test')
-        # Satisfied, she goes back to sleep.
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-    
-    
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,3 @@
-from django.urls import resolve
 from django.test import TestCase
 from lists.models import Item
 
@@ -10,38 +9,24 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-        
-        # print(response.content.decode())
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
-
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-        
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
 
 
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
 
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
 
 
 class ItemModelTest(TestCase):
@@ -62,3 +47,33 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+
+    def test_user_list_template(self):
+        response = self.client.get('lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+       
+    def test_displayes_all_views(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+        
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        
+class NewListTest(TestCase):
+    
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    def test_redirects_after_post(self):
+        response = self.client.get('lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, 'lists/the-only-list-in-the-world/')
+        
