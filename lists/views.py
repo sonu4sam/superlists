20dd2e78d2791595dafa17886640779bfe709509
sonu4sam <sonu4sam@outlook.com>
2023-07-05 11:45:44 -0500

    unittest fail 1

--- lists/views.py
+++ lists/views.py
@@ -12,14 +12,14 @@ def home_page(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/') 
+    return redirect('view_list', list_id=list_.id) 
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+    return redirect('view_list', list_id=list_.id)
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.get(list=list_)
-    return render(request, 'list.html', {'list': list_})
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_, 'items': items})
